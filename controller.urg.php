28c4717db71b3ba12ffa1934dbe0a35035dd5cdc
Marius Mandal <mariusmandal@gmail.com>
2013-10-21 12:06:43 +0200

    Urg-kontroller select data

---
+++ controller.urg.php
@@ -0,0 +1,31 @@
+<?php
+
+$sql = new SQL("SELECT `con`.`id`,
+					   `pl`.`pl_fylke`,
+					   `pl`.`pl_name`
+				FROM `smartukm_contacts` AS `con`
+				LEFT JOIN `smartukm_rel_pl_ab` AS `rel` ON (`rel`.`ab_id` = `con`.`id`)
+				LEFT JOIN `smartukm_place` AS `pl` ON (`pl`.`pl_id` = `rel`.`pl_id`)
+				WHERE `system_locked` = 'true'
+				AND `email` LIKE '%@urg.ukm.no%'
+				AND `season` = '#season'",
+			array('season' => get_option('season')));
+				
+$res = $sql->run();
+
+$kontakter = array();
+
+while( $r = mysql_fetch_assoc( $res ) ) {
+	$object = new kontakt( $r['id'] );
+	
+	$kontakt = new StdClass;
+	$kontakt->fylke = new StdClass;
+	
+	$kontakt->navn = $object->firstname;
+	$kontakt->fylke->id = $r['pl_fylke'];
+	$kontakt->fylke->navn = $r['pl_name'];
+	$kontakt->bilde = $object->image();
+	$kontakter[] = $kontakt;
+}
+
+var_dump($kontakter);
