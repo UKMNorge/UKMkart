856bce4ed5149ba829bb4ebe652157b3df2ba5e0
Marius Mandal <mariusmandal@gmail.com>
2013-10-21 20:57:57 +0200

    urg meet new return

--- controller_urg.inc.php
+++ controller_urg.inc.php
@@ -2,7 +2,7 @@
 require_once('UKM/kontakt.class.php');
 require_once('UKM/monstring.class.php');
 
-$mapUrl = gen_map('URG', 'urg.ukm.no');
+$mapinfo = gen_map('URG', 'urg.ukm.no');
 
 global $UKMkart_GD_LOG;
-$infos = array('kontakter' => $kontakter, 'log' => $UKMkart_GD_LOG, 'kart' => $mapUrl);
+$infos = array('kontakter' => $mapinfo->kontakter, 'log' => $UKMkart_GD_LOG, 'kart' => $mapinfo->url);
