0ef66ec2192d734ea98a50c73c978576496e4352
Marius Mandal <mariusmandal@gmail.com>
2014-12-03 14:20:02 +0100

    syntax

--- UKMkart.php
+++ UKMkart.php
@@ -26,7 +26,7 @@ function UKMkart_network_dash_messages( $MESSAGES ) {
 		if( isset( $_GET['debug'] ) ) {
 			var_dump( $image );
 		}
-		if( empty( $image ) || strpos( $image, 'placeholder/person' ) != false ) {
+		if( empty( $image ) || strpos( $image, 'placeholder/person' ) != false ) {
 			$ERROR++;
 		}
 	}
